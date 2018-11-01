f325b72fd512224bf730b390320ad5fcafd7a6e7
Joe Miller <joemillerinportland@gmail.com>
2018-11-01 04:53:27 -0700

    Added Java and Python Hello Worlds!

---
+++ Java/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Joseph Miller
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args Commandline arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
